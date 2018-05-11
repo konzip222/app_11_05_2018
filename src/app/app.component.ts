8b1edc7ba6dd41a970ad4138b9484ec518a6fd02
kuba <kuba>
2018-05-11 15:09:29 +0200

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,28 @@
 import { Component } from '@angular/core';
+import { ClockComponent } from './components/clock/clock.component';
+import { Title }     from '@angular/platform-browser';
+
+interface TimerReportModel{
+    Id: number;   
+    IssueId: number;
+    Issue: Issue;
+    UserId: number;
+    User: User;
+    StartTime: Date;
+    EndTime: Date;
+}
+
+interface Issue{
+    Key :string;
+    IssueName :string;  
+}
+
+interface User{
+    UserId: number;
+    EmailAddress: string;
+    FullName: string;
+}
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +30,36 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+
   title = 'app';
+  test = new Date();
+  dateInputStart = new Date(this.test.getTime()-3600000);
+  dateInputStop = new Date(this.test.getTime()-1600000);  
+  newTaskName = "new task";
+
+  inputObject = {
+    initialDate : new Date(this.test.getTime()-3600000),
+    taskName : "new task"
+  };
+
+  newUser = {
+    UserId: 31,
+    EmailAddress: "konzip22@o2.pl",
+    FullName: "andrzejKowalski"
+  }
+
+  newIssue = {
+    Key :"[ZM123]",
+    IssueName :"new asewome long Issue"  
+  }
+
+  inputObjectNew = {
+    Id: 123123,   
+    IssueId: 12,
+    Issue: this.newIssue,
+    UserId: 33412321,
+    User: this.newUser,
+    StartTime: this.dateInputStart,
+    EndTime: this.dateInputStop
+  }
 }
