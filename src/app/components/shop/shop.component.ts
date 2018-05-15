7a2d8b6c57fcba3a16cf6d2afed5d019d7ff6380
kuba <kuba>
2018-05-15 15:19:57 +0200

    15_05_2018

--- src/app/components/shop/shop.component.ts
+++ src/app/components/shop/shop.component.ts
@@ -11,9 +11,23 @@ export class ShopComponent implements OnInit {
   constructor(private _shopDataService: ShopDataService) { }
 
   goals = [];
+  goalText = "Some text";
+  itemCount = 0;
 
   ngOnInit() {
     this._shopDataService.goal.subscribe(res => this.goals = res);
+  }
+
+  addItem() {
+    this.goals.push(this.goalText);
+    this.goalText += this.itemCount;
+    this._shopDataService.changeGoal(this.goals);
+    this.itemCount = this.goals.length;    
+  }
+
+  removeItem(i) {
+    this.goals.splice(i, 1);
+    this._shopDataService.changeGoal(this.goals);
   }
 
 }
--- src/app/services/shop-data.service.ts
+++ src/app/services/shop-data.service.ts
@@ -12,4 +12,5 @@ export class ShopDataService {
   changeGoal(goal) {
     this.goals.next(goal)
   }
+
 }
